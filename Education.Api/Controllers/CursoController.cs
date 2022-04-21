9a7d1f1494a64e71d48b6c754cd34da7f3f6026e
jorge.lopez <jorge.lopez@tarlogic.com>
2022-04-21 10:58:09 +0200

    Se añade el controller para consulta y creacion de cursos junto con la configuracion de servicios y dependency injection

---
+++ Education.Api/Controllers/CursoController.cs
@@ -0,0 +1,33 @@
+﻿using Education.Application.DTO;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Education.Application.Cursos.CreateCursoCommand;
+using static Education.Application.Cursos.GetCursoQuery;
+
+namespace Education.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public CursoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CursoDTO>>> Get()
+        {
+            return await _mediator.Send(new GetCursoQueryRequest());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Post([FromBody] CreateCursoCommandRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+    }
+}
--- Education.Api/Program.cs
+++ Education.Api/Program.cs
@@ -1,8 +1,32 @@
+using Education.Application.Cursos;
+using Education.Persistence;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<EducationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddMediatR(typeof(GetCursoQuery).Assembly);
+
+builder.Services
+    .AddControllers()
+    .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<CreateCursoCommand>());
+
+builder.Services.AddAutoMapper(typeof(GetCursoQuery).Assembly);
+
+builder.Services
+    .AddCors(cors => cors.AddPolicy("corsApp", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    }));
 
 var app = builder.Build();
 
